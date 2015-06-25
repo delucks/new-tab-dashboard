651ad57823c1885b7d678bafa9d55c768bbd2bde
delucks <jamie.delucks@gmail.com>
2015-06-25 15:12:08 -0400

    Fully swapped to chartkick. Wrote a very basic landing page, with some of the stats read upon page load. Will continue to iterate on that landing page until its up to scratch

--- dashboard/newtab-server.py
+++ dashboard/newtab-server.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, render_template, Blueprint
+import shlex
+from subprocess import Popen, PIPE
 import logging
-import threading
 import urllib2
-import sqlite3
-import time
 import chartkick
+import datetime
 app = Flask(__name__)
 
 ck = Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_path='/static')
 app.register_blueprint(ck, url_prefix='/ck')
 app.jinja_env.add_extension("chartkick.ext.charts")
-
-HALT_EVENT = threading.Event()
 
 '''
 functionality
@@ -23,10 +21,6 @@ design
     simple white or grey background, colorful graphs
     some kind of a JS graphing library for the frontend
     read most of the info directly off proc so it's fast
-poll proc every few seconds, put information into a DB w/ timestamp
-    frontend is a separate thread/process that renders the app
-    queries the DB through JS for information
-    need to periodically remove entries older than a day or so from the DB
 '''
 
 '''
@@ -46,7 +40,9 @@ def gen_nonempty(iterative):
         if item.strip() != '':
             yield item
 
-# read /proc/cpuinfo, parse the data into a dict
+'''
+read /proc/cpuinfo, parse the data into a dict
+'''
 def proc_cpuinfo():
     logging.info('utility,proc_cpuinfo')
     with open('/proc/cpuinfo', 'r') as info:
@@ -59,20 +55,31 @@ def proc_cpuinfo():
                 information['proc_count'] += 1
             elif cat == 'model name':
                 information['model'] = datum
-            elif cat == 'BogoMIPS':
+            elif cat.lower() == 'bogomips':
                 information['speed'] = datum
-            elif cat == 'Hardware':
+            elif cat.lower() == 'hardware':
                 information['name'] = datum
+            elif cat == 'cache size':
+                information['cache'] = datum
+            elif cat == 'address sizes':
+                information['address'] = datum
     return information
 
-# read loadavg from proc
+'''
+read loadavg from proc
+'''
 def proc_load():
     logging.info('utility,proc_load')
     with open('/proc/loadavg', 'r') as info:
         data = info.read().split()
-        return {'1': data[0], '5': data[1], '15': data[2]}
+        now = datetime.datetime.now()
+        five = now - datetime.timedelta(minutes=5)
+        fifteen = now - datetime.timedelta(minutes=15)
+        return {str(now): data[0], str(five): data[1], str(fifteen): data[2]}
 
-# read some memory stats from proc. all numbers are in kilobytes
+'''
+read some memory stats from proc. all numbers are in kilobytes
+'''
 def proc_mem():
     logging.info('utility,proc_mem')
     with open('/proc/meminfo', 'r') as info:
@@ -81,85 +88,78 @@ def proc_mem():
             sep = [i.strip() for i in memstat.split(':')]
             cat = sep[0]
             datum = sep[1].rstrip(' kB')
-            if cat == 'MemTotal':
-                information['total'] = datum
-            elif cat == 'MemFree':
+            #if cat == 'MemTotal':
+            #    information['total'] = datum
+            if cat == 'MemFree':
                 information['free'] = datum
-            elif cat == 'Buffers':
-                information['buffers'] = datum
+            #elif cat == 'Buffers':
+            #    information['buffers'] = datum
             elif cat == 'Cached':
                 information['cache'] = datum
-            elif cat == 'SwapCached':
-                information['swap'] = datum
+            #elif cat == 'SwapCached':
+            #    information['swap'] = datum
             elif cat == 'Active':
                 information['active'] = datum
     return information
 
-'''
-database logic
-'''
+def weather():
+    pass
 
-# connect to the database and do the needful
-def init_db(schemaname='newtab-schema.sql',
-        dbname='newtab-db.sqlite3'):
-    logging.info('database,init_db')
-    db_conn = sqlite3.connect(dbname)
-    cur = db_conn.cursor()
-    with open(schemaname, 'r') as f:
-        cur.executescript(f.read())
-    db_conn.close()
-
-# worker thread which collects statistics every five minutes
-def db_worker():
-    dbname='newtab-db.sqlite3'
-    while not HALT_EVENT.is_set():
-        memstats = proc_mem()
-        load = proc_load()
-        db_conn = sqlite3.connect(dbname)
-        cur = db_conn.cursor()
-        logging.info('Periodic update')
-        cur.execute("INSERT INTO Stats values('{l1}','{l5}','{l15}','{mT}','{mB}','{mF}','{mC}','{mS}','{mA}',datetime('now'))".format(l1=load['1'],
-            l5=load['5'],
-            l15=load['15'],
-            mT=memstats['total'],
-            mB=memstats['buffers'],
-            mF=memstats['free'],
-            mC=memstats['cache'],
-            mS=memstats['swap'],
-            mA=memstats['active']))
-        cur.close()
-        db_conn.close()
-        #time.sleep(60)
+'''
+read from spotify's DBUS api
+'''
+def now_playing():
+    #TODO: Rewrite this using pydbus instead of using a Popen() call
+    remove_junk = lambda x: x.split('"')[1]
+    cmd = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'"
+    proc = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+    stdout, stderr = proc.communicate()
+    strs = [line.strip() for line in stdout.splitlines() if 'string' in line]
+    for idx, item in enumerate(strs):
+        if 'xesam:artist' in item:
+            artist = strs[idx+1]
+        elif 'xesam:album' in item:
+            album = strs[idx+1]
+        elif 'xesam:title' in item:
+            title = strs[idx+1]
+        elif 'mpris:artUrl' in item:
+            art_link = strs[idx+1]
+    return [remove_junk(item) for item in [artist, album, title, art_link]]
 
 '''
 application logic
 '''
 
+def get_settings():
+    return {
+            "backgroundColor": "#111",
+            "colors": ["#526f33", "#E84F4F", "#9B64FB"],
+            "titleColor": "#fff",
+            "borderColor": "#333",
+            "axisColor": "#333",
+            "axisBackgroundColor": "#333",
+           }
+
 '''
 this method renders a template of graphs without data
-to include data queried from the sqlite3 database
-using chart.js and skeleton.css for frontend
+using chartkick and skeleton.css for frontend
 '''
 @app.route('/')
 def render_dashboard():
-    return render_template('index.html', data={'Foo':5, 'Bar': 10, 'Baz': 12})
+    cpu_info = proc_cpuinfo()
+    print cpu_info
+    return render_template('index.html',
+            load_data=proc_load(),
+            mem_data=proc_mem(),
+            settings=get_settings(),
+            music=now_playing(),
+            cpu_info=cpu_info)
 
 if (__name__ == '__main__'):
     import argparse
     p = argparse.ArgumentParser(description='run the server with different behavior')
-    p.add_argument('-t', '--test-db', help='verbosely output data into a db file')
-    p.add_argument('-a', '--app-only', help='only run the flask app, don\'t spawn the db_worker thread', action='store_true')
-    p.add_argument('-h', '--bind-host', help='set the host to run on', default='127.0.0.1')
+    p.add_argument('-b', '--bind-host', help='set the host to run on', default='127.0.0.1')
     p.add_argument('-p', '--port', help='set the port to run on', type=int, default=9001)
     args = p.parse_args()
-    init_db()
-    logging.basicConfig(level=logging.INFO, filename='debug.log')
-    if args.test_db:
-        db_worker()
-        HALT_EVENT.set()
-    elif args.app_only:
-        app.run(host=args.bind_host, debug=True, port=args.port)
-    else:
-        worker = threading.Thread(target=db_worker)
-        worker.start()
-        app.run(host=args.bind_host, debug=True, port=args.port) # this blocks
+    #logging.basicConfig(level=logging.INFO, filename='debug.log')
+    app.run(host=args.bind_host, debug=True, port=args.port)
