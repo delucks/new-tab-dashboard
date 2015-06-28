104f300c56ee3c62fbaa55bb4d230654ccbf3076
delucks <jamie.delucks@gmail.com>
2015-06-28 17:10:36 -0400

    Added the rest of the classes. Working on config parsing

--- newtab-server.py
+++ newtab-server.py
@@ -1,4 +1,5 @@
 import flask
+import argparse
 import getpass
 import logging
 import urllib2
@@ -6,7 +7,6 @@ import chartkick
 import importlib
 import datetime
 import ConfigParser
-app = flask.Flask(__name__)
 
 try:
     import dbus
@@ -14,9 +14,16 @@ try:
 except ImportError:
     APP_WITHOUT_DBUS=True
 
+app = flask.Flask(__name__)
 ck = flask.Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_path='/static')
 app.register_blueprint(ck, url_prefix='/ck')
 app.jinja_env.add_extension("chartkick.ext.charts")
+
+'''
+Global Utility Functions
+'''
+
+gen_nonempty = lambda it: [element for element in it if element.strip() != '']
 
 '''
 holds 1+ Column objects
@@ -24,12 +31,33 @@ defines a method to autocalculate column
 widths and runs it before passing the columns on
 '''
 class Row(object):
-    def __init__(self, num_columns, columns):
-        self.num_columns = num_columns
+    def __init__(self, columns):
         self.columns = columns
+        self.translation = {
+                1: 'one',
+                2: 'two',
+                3: 'three',
+                4: 'four',
+                5: 'five',
+                6: 'six',
+                7: 'seven',
+                8: 'eight',
+                9: 'nine',
+               10: 'ten',
+               11: 'eleven',
+               12: 'twelve'}
 
-    def calculate_column_width(self):
-        pass
+    def change_column_width(self):
+        each = int(12 / len(self.columns))
+        remainder = 12 % len(self.columns)
+        first = each + remainder
+        each = self.translation[each] # make it english
+        first = self.translation[first]
+        for idx, col in enumerate(self.columns):
+            if idx == 0:
+                col.set_size(first)
+            else:
+                col.set_size(each)
 
 '''
 Base class for all Columns
@@ -42,6 +70,9 @@ class Column(object):
 
     def __init__(self, size):
         self.size = size
+
+    def set_size(self, new_size):
+        self.size = new_size
 
     def get_data(self):
         raise NotImplementedError('Must subclass Column!')
@@ -77,6 +108,77 @@ class MemoryColumn(Column):
     def get_data(self):
         return {'mem_data': self.proc_mem()}
 
+
+'''
+read /proc/cpuinfo, parse the data into a dict
+'''
+class CPUInfoColumn(Column):
+    coltype = "CPUInfo"
+
+    def proc_cpuinfo(self):
+        logging.info('utility,proc_cpuinfo')
+        with open('/proc/cpuinfo', 'r') as info:
+            information = {'proc_count': 0}
+            for entry in gen_nonempty(info.readlines()):
+                sep = [i.strip() for i in entry.split(':')]
+                cat = sep[0]
+                datum = sep[1]
+                if cat == 'processor':
+                    information['proc_count'] += 1
+                elif cat == 'model name':
+                    information['model'] = datum
+                elif cat.lower() == 'bogomips':
+                    information['speed'] = datum
+                elif cat.lower() == 'hardware':
+                    information['name'] = datum
+                elif cat == 'cache size':
+                    information['cache'] = datum
+                elif cat == 'address sizes':
+                    information['address'] = datum
+        return information
+
+    def get_data(self):
+        return {'cpu_info': self.proc_cpuinfo()}
+
+'''
+read loadavg from proc
+'''
+class LoadColumn(Column):
+    coltype = "Load"
+
+    def proc_load(self):
+        logging.info('utility,proc_load')
+        with open('/proc/loadavg', 'r') as info:
+            data = info.read().split()
+            now = datetime.datetime.now()
+            five = now - datetime.timedelta(minutes=5)
+            fifteen = now - datetime.timedelta(minutes=15)
+            return {str(now): data[0], str(five): data[1], str(fifteen): data[2]}
+
+    def get_data(self):
+        return {'load': self.proc_load()}
+
+'''
+read from spotify's DBUS api
+'''
+class MusicColumn(Column):
+    coltype = "Music"
+
+    def now_playing(self):
+        bus = dbus.SessionBus()
+        try:
+            player = bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
+            properties_manager = dbus.Interface(player, 'org.freedesktop.DBus.Properties')
+            meta = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+            meta['xesam:artist'] = meta['xesam:artist'][0] # remove the unnecessary array
+            return [v for k, v in meta.iteritems() if ('album' in k or 'title' in k or 'artist' in k or 'art' in k)]
+        except dbus.DBusException:
+            # returning None will prevent this whole draw element from being displayed
+            return None
+
+    def get_data(self):
+        return {'music': self.now_playing()}
+
 '''
 This is as blank as you can get, a static column
 '''
@@ -98,8 +200,15 @@ def parse_config(config_file='newtab-server.cfg'):
     rows = []
     m = MemoryColumn(size="six")
     l = LinksColumn(size="six")
-    r1 = Row(2, [m, l])
+    r1 = Row([m, l])
+    l = LoadColumn(size="twelve")
     rows.append(r1)
+    r2 = Row([l])
+    rows.append(r2)
+    s = MusicColumn(size="six")
+    l = LinksColumn(size="six")
+    r3 = Row([l, s])
+    rows.append(r3)
     #for row_name in c.sections():
     #    for col_name in c.options(row_name):
     #        opt = c.get(row_name, col_name)
@@ -108,63 +217,6 @@ def parse_config(config_file='newtab-server.cfg'):
     #        rows.append(row)
     #return numrows
     return rows
-
-def gen_nonempty(iterative):
-    for item in iterative:
-        if item.strip() != '':
-            yield item
-
-'''
-read /proc/cpuinfo, parse the data into a dict
-'''
-def proc_cpuinfo():
-    logging.info('utility,proc_cpuinfo')
-    with open('/proc/cpuinfo', 'r') as info:
-        information = {'proc_count': 0}
-        for entry in gen_nonempty(info.readlines()):
-            sep = [i.strip() for i in entry.split(':')]
-            cat = sep[0]
-            datum = sep[1]
-            if cat == 'processor':
-                information['proc_count'] += 1
-            elif cat == 'model name':
-                information['model'] = datum
-            elif cat.lower() == 'bogomips':
-                information['speed'] = datum
-            elif cat.lower() == 'hardware':
-                information['name'] = datum
-            elif cat == 'cache size':
-                information['cache'] = datum
-            elif cat == 'address sizes':
-                information['address'] = datum
-    return information
-
-'''
-read loadavg from proc
-'''
-def proc_load():
-    logging.info('utility,proc_load')
-    with open('/proc/loadavg', 'r') as info:
-        data = info.read().split()
-        now = datetime.datetime.now()
-        five = now - datetime.timedelta(minutes=5)
-        fifteen = now - datetime.timedelta(minutes=15)
-        return {str(now): data[0], str(five): data[1], str(fifteen): data[2]}
-
-'''
-read from spotify's DBUS api
-'''
-def now_playing():
-    bus = dbus.SessionBus()
-    try:
-        player = bus.get_object('org.mpris.MediaPlayer2.spotify', '/org/mpris/MediaPlayer2')
-        properties_manager = dbus.Interface(player, 'org.freedesktop.DBus.Properties')
-        meta = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-        meta['xesam:artist'] = meta['xesam:artist'][0] # remove the unnecessary array
-        return [v for k, v in meta.iteritems() if ('album' in k or 'title' in k or 'artist' in k or 'art' in k)]
-    except dbus.DBusException:
-        # returning None will prevent this whole draw element from being displayed
-        return None
 
 def get_settings():
     return {
@@ -209,10 +261,11 @@ def get_rows():
     flask.g.rows = rows
 
 if (__name__ == '__main__'):
-    import argparse
+    # handle arguments
     p = argparse.ArgumentParser(description='run the server with different behavior')
     p.add_argument('-b', '--bind-host', help='set the host to run on', default='127.0.0.1')
     p.add_argument('-p', '--port', help='set the port to run on', type=int, default=9001)
+    p.add_argument('-c', '--configfile', help='specify an alternate config file', default='newtab.cfg')
     args = p.parse_args()
-    # this should hopefully allow us to bind rows to the global 'g'
+    # initialize the application
     app.run(host=args.bind_host, debug=True, port=args.port)
